a1a256813c2f08891055ca4b7b8fc1059dfd96ef
Maic Miller <maicmiller42@gmail.com>
2021-08-02 22:52:44 -0300

    adicionando os métodos get() e getNo.

--- listaDuplamenteEncadeada/src/main/java/com/github/maicmiller/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/src/main/java/com/github/maicmiller/ListaDuplamenteEncadeada.java
@@ -13,6 +13,17 @@ public class ListaDuplamenteEncadeada<T> {
         this.tamanhoLista = 0;
     }
 
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo; // O nó auxiliar recebe o primeiro nó, ou seja começa do primeiro nó...
+        for (int i =0; (i < index) && (noAuxiliar != null); i++){ // Enquanto isso for true, o laço continua a percorrer e avançar na milha lista...
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return  noAuxiliar;
+    }
 
     public int size(){
         return tamanhoLista;
