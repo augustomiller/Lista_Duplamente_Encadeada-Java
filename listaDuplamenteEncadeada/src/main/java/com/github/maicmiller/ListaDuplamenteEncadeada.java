9522102221827694e6e0b8278fa41828d3933015
Maic Miller <maicmiller42@gmail.com>
2021-08-02 23:03:26 -0300

    adicionando os métodos add().

--- listaDuplamenteEncadeada/src/main/java/com/github/maicmiller/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/src/main/java/com/github/maicmiller/ListaDuplamenteEncadeada.java
@@ -17,6 +17,20 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T elemento){ // Recebe o conteúdo do tipo genérico como parâmetro...
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo); // Esse ultimo nó aponta para o antigo ultimo nó da lista...
+        if (primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo; // O nó auxiliar recebe o primeiro nó, ou seja começa do primeiro nó...
         for (int i =0; (i < index) && (noAuxiliar != null); i++){ // Enquanto isso for true, o laço continua a percorrer e avançar na milha lista...
@@ -26,6 +40,6 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size(){
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 }
